be74cff8714087dd0d4684eb143a0de284147f90
João Pedro <jpedrodaibello1999@outlook.com>
2021-09-14 09:09:45 -0300

    Aula 1

---
+++ lib/modules/splash/splash_page.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: Get.width,
+          height: Get.height,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Image.asset('assets/images/logo.png'),
+        ),
+      ),
+    );
+  }
+}
